42e87eb6b5924b7114fc276fc2b110f3a19d64a3
Sunitha K <Sunitha K@DESKTOP-D53LLD0>
2023-07-02 09:55:25 +0530

    initial commit

---
+++ SeriesExamples/src/com/example/ExampleOne.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.Scanner;
+//Concatenating Strings:
+public class ExampleOne {
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "World";
+		String result = str1 + " " + str2;
+		System.out.println(result); // Output: "Hello World"
+		
+		}
+	}
+
+
